b10276177fe32fdff6dfd6879a0267b9e631c141
Owen L <owenluu111@gmail.com>
2022-06-12 22:12:43 -0500

    added some css to the hero.style.scss file so the image can be viewable behind the text on the banner

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,7 +3,7 @@ import './hero.styles.scss';
 
 const Hero = () => {
   return (
-    <section className="hero is-info is-large">
+    <section className="hero is-info is-large hero-image">
       <div className="hero-body">
         <div className="container">
           <h1 className="title">
