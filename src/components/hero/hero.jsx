f602f267e5129921ffb9c332d3decd17c96cdd01
Owen L <owenluu111@gmail.com>
2022-06-12 22:03:38 -0500

    Imported a Hero banner from Bulma.io and configued it in the hero.jsx file and imported it into the apps.js file from the hero component

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './hero.styles.scss';
+
+const Hero = () => {
+  return (
+    <section className="hero is-info is-large">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            Bags Reimagined For Modern Life.
+          </h1>
+          <h2 className="shop-now-btn">
+            <button className='button is-black' id='shop-now'>
+            SHOP NOW
+            </button>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
