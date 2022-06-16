8830456c31764a520cacc02ae7513e881a39e6d5
Owen L <owenluu111@gmail.com>
2022-06-16 00:15:23 -0500

    added a layout in jsx for the single-product section. Added a check to see if there is a product to load.

--- src/components/single-product/single-product.jsx
+++ src/components/single-product/single-product.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { withRouter } from 'react-roouter-dom';
+import { withRouter } from 'react-router-dom';
 import { ProductsContext } from '../../context/products-context';
-import Layout from '../../shared/layout';
+import Layout from '../shared/layout';
 import './single-product.styles.scss';
 
-const = SingleProduct = ({ match, history: { push } }) => {
+const SingleProduct = ({ match, history: { push } }) => {
   const { products } = useContext(ProductsContext);
   const { id } = match.params; 
   const [product, setProduct] = useState(null);
@@ -18,21 +18,37 @@ const = SingleProduct = ({ match, history: { push } }) => {
     
     setProduct(product)
   });
-
+  // if there is no product atm we will use the check above and when it finds a product it will show the product
+  if (!product) { return null }
   const { imageUrl, title, price, description } = product;
   return (
     <Layout>
-      <div>
-        <div>
+      <div className='single-product-container'>
+        <div className='product-image'>
           <img src={imageUrl} alt='product'/>
         </div>
-        <div>
-          <div>
+        <div className='product-details'>
+          <div className='name-price'>
             <h3>{title}</h3>
             <p>{price}</p>
+          </div>
+          <div className='add-to-cart-btns'>
+            <button className='button is-white nomad-btn' id='btn-white-outline'>
+              ADD TO CART
+            </button>
+            <button className='button is-black nomad-btn' id='btn-white-outline'>
+              PROCEED TO CHECKOUT
+            </button>
+          </div>
+          <div className='product-description'>
+            <p>
+              { description }
+            </p>
           </div>
         </div>
       </div>
     </Layout>
   );
 }
+
+export default withRouter(SingleProduct);
