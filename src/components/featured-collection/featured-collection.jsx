3b2229b08af2504932353b46774b07f6c1b8f806
Owen L <owenluu111@gmail.com>
2022-06-15 22:14:24 -0500

    fixed a shit ton of typos made changes to the shop.jsx file and apps.js file for the route as well as the app.scss for the css from Bulma

--- src/components/featured-collection/featured-collection.jsx
+++ src/components/featured-collection/featured-collection.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
-import { ProductContext } from '../../context/products-context';
+import { ProductsContext } from '../../context/products-context';
 import FeaturedProduct from '../shared/featured-product';
 
 const FeaturedCollection = () => {
-  const { products } = useContext(ProductContext);
+  const { products } = useContext(ProductsContext);
   const productItems = products.filter((product, i) => i < 4).map(product => (
     <FeaturedProduct {...product} key={product.id} />
   ));
