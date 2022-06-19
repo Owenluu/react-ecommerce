6327734ae26cf26bb21432b120edf381f0f89479
Owen L <owenluu111@gmail.com>
2022-06-18 23:43:55 -0500

    fixed an error on the export for cart-icon.jsx file

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import shoppingBag from '../../assets/shopping-bag.png';
-import CartContextProvider from "../../context/cart-context";
+import { CartContext } from "../../context/cart-context";
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
